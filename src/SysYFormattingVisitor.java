66f03c787e1155ee620aaf946f4272c9af60f857
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-03-12 16:21:48 +0800

    > make 231250190 linan Linux linan-VMware-Virtual-Platform 6.11.0-17-generic #17~24.04.2-Ubuntu SMP PREEMPT_DYNAMIC Mon Jan 20 22:48:29 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:21:48 up  1:53,  1 user,  load average: 0.13, 0.28, 0.33 e425cbedd35fa311835a7d3dfb07a321efc1b10f

--- src/SysYFormattingVisitor.java
+++ src/SysYFormattingVisitor.java
@@ -57,12 +57,64 @@ public class SysYFormattingVisitor extends SysYParserBaseVisitor<String> {
         return sb.toString();
     }
 
-    // Other visit methods for all rule contexts...
+    @Override
+    public String visitBType(SysYParser.BTypeContext ctx) {
+        return ctx.getText();
+    }
+
+    @Override
+    public String visitVarDecl(SysYParser.VarDeclContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(getIndent()).append(visit(ctx.bType())).append(" ");
+
+        for (int i = 0; i < ctx.varDef().size(); i++) {
+            sb.append(visit(ctx.varDef(i)));
+            if (i < ctx.varDef().size() - 1) {
+                sb.append(", ");
+            }
+        }
+        sb.append(";");
+        return sb.toString();
+    }
+
+    @Override
+    public String visitVarDef(SysYParser.VarDefContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ctx.IDENT().getText());
+
+        for (SysYParser.ConstExpContext expCtx : ctx.constExp()) {
+            sb.append("[").append(visit(expCtx)).append("]");
+        }
+
+        if (ctx.ASSIGN() != null) {
+            sb.append(" = ").append(visit(ctx.initVal()));
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public String visitInitVal(SysYParser.InitValContext ctx) {
+        if (ctx.exp() != null) {
+            return visit(ctx.exp());
+        } else {
+            StringBuilder sb = new StringBuilder();
+            sb.append("{");
+            for (int i = 0; i < ctx.initVal().size(); i++) {
+                sb.append(visit(ctx.initVal(i)));
+                if (i < ctx.initVal().size() - 1) {
+                    sb.append(", ");
+                }
+            }
+            sb.append("}");
+            return sb.toString();
+        }
+    }
 
     @Override
     public String visitFuncDef(SysYParser.FuncDefContext ctx) {
         StringBuilder sb = new StringBuilder();
-        sb.append(getIndent()).append(visit(ctx.funcType())).append(" ");
+        sb.append(visit(ctx.funcType())).append(" ");
         sb.append(ctx.IDENT().getText()).append("(");
 
         if (ctx.funcFParams() != null) {
@@ -71,6 +123,23 @@ public class SysYFormattingVisitor extends SysYParserBaseVisitor<String> {
 
         sb.append(") ");
         sb.append(visit(ctx.block()));
+        return sb.toString();
+    }
+
+    @Override
+    public String visitFuncType(SysYParser.FuncTypeContext ctx) {
+        return ctx.getText();
+    }
+
+    @Override
+    public String visitFuncFParams(SysYParser.FuncFParamsContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ctx.funcFParam().size(); i++) {
+            sb.append(visit(ctx.funcFParam(i)));
+            if (i < ctx.funcFParam().size() - 1) {
+                sb.append(", ");
+            }
+        }
         return sb.toString();
     }
 
@@ -85,8 +154,13 @@ public class SysYFormattingVisitor extends SysYParserBaseVisitor<String> {
         }
 
         indentLevel--;
-        sb.append(getIndent()).append("}\n");
+        sb.append(getIndent()).append("}");
         return sb.toString();
+    }
+
+    @Override
+    public String visitBlockItem(SysYParser.BlockItemContext ctx) {
+        return visit(ctx.getChild(0));
     }
 
     @Override
@@ -94,7 +168,27 @@ public class SysYFormattingVisitor extends SysYParserBaseVisitor<String> {
         StringBuilder sb = new StringBuilder();
         sb.append(getIndent());
 
-        if (ctx.IF() != null) {
+        if (ctx.lVal() != null && ctx.ASSIGN() != null) {
+            // Assignment statement
+            sb.append(visit(ctx.lVal())).append(" = ").append(visit(ctx.exp())).append(";\n");
+        } else if (ctx.SEMICOLON() != null && ctx.exp() == null) {
+            // Empty statement
+            sb.append(";\n");
+        } else if (ctx.exp() != null && ctx.SEMICOLON() != null) {
+            // Expression statement
+            sb.append(visit(ctx.exp())).append(";\n");
+        } else if (ctx.block() != null) {
+            // Block statement
+            sb.append(visit(ctx.block())).append("\n");
+        } else if (ctx.RETURN() != null) {
+            // Return statement
+            sb.append("return");
+            if (ctx.exp() != null) {
+                sb.append(" ").append(visit(ctx.exp()));
+            }
+            sb.append(";\n");
+        } else if (ctx.IF() != null) {
+            // If statement
             sb.append("if (").append(visit(ctx.cond())).append(") ");
 
             if (ctx.stmt(0).block() != null) {
@@ -121,13 +215,107 @@ public class SysYFormattingVisitor extends SysYParserBaseVisitor<String> {
                     indentLevel--;
                 }
             }
+        } else if (ctx.WHILE() != null) {
+            // While statement
+            sb.append("while (").append(visit(ctx.cond())).append(") ");
+            if (ctx.stmt(0).block() != null) {
+                sb.append(visit(ctx.stmt(0)));
+            } else {
+                sb.append("\n");
+                indentLevel++;
+                sb.append(visit(ctx.stmt(0)));
+                indentLevel--;
+            }
+        } else if (ctx.BREAK() != null) {
+            sb.append("break;\n");
+        } else if (ctx.CONTINUE() != null) {
+            sb.append("continue;\n");
         }
-        // Handle other statement types...
 
         return sb.toString();
     }
 
-    // Methods for expressions, binary ops, etc.
+    @Override
+    public String visitExp(SysYParser.ExpContext ctx) {
+        return visit(ctx.addExp());
+    }
+
+    @Override
+    public String visitCond(SysYParser.CondContext ctx) {
+        return visit(ctx.lOrExp());
+    }
+
+    @Override
+    public String visitLVal(SysYParser.LValContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        sb.append(ctx.IDENT().getText());
+
+        for (SysYParser.ExpContext expCtx : ctx.exp()) {
+            sb.append("[").append(visit(expCtx)).append("]");
+        }
+
+        return sb.toString();
+    }
+
+    @Override
+    public String visitPrimaryExp(SysYParser.PrimaryExpContext ctx) {
+        if (ctx.L_PAREN() != null) {
+            return "(" + visit(ctx.exp()) + ")";
+        } else if (ctx.lVal() != null) {
+            return visit(ctx.lVal());
+        } else {
+            return visit(ctx.number());
+        }
+    }
+
+    @Override
+    public String visitNumber(SysYParser.NumberContext ctx) {
+        return ctx.INTEGER_CONST().getText();
+    }
+
+    @Override
+    public String visitUnaryExp(SysYParser.UnaryExpContext ctx) {
+        if (ctx.primaryExp() != null) {
+            return visit(ctx.primaryExp());
+        } else if (ctx.IDENT() != null) {
+            StringBuilder sb = new StringBuilder();
+            sb.append(ctx.IDENT().getText()).append("(");
+            if (ctx.funcRParams() != null) {
+                sb.append(visit(ctx.funcRParams()));
+            }
+            sb.append(")");
+            return sb.toString();
+        } else {
+            return ctx.getChild(0).getText() + visit(ctx.unaryExp());
+        }
+    }
+
+    @Override
+    public String visitFuncRParams(SysYParser.FuncRParamsContext ctx) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < ctx.param().size(); i++) {
+            sb.append(visit(ctx.param(i)));
+            if (i < ctx.param().size() - 1) {
+                sb.append(", ");
+            }
+        }
+        return sb.toString();
+    }
+
+    @Override
+    public String visitParam(SysYParser.ParamContext ctx) {
+        return visit(ctx.exp());
+    }
+
+    @Override
+    public String visitMulExp(SysYParser.MulExpContext ctx) {
+        if (ctx.getChildCount() == 1) {
+            return visit(ctx.unaryExp());
+        } else {
+            return visit(ctx.mulExp()) + " " + ctx.getChild(1).getText() + " " + visit(ctx.unaryExp());
+        }
+    }
+
     @Override
     public String visitAddExp(SysYParser.AddExpContext ctx) {
         if (ctx.getChildCount() == 1) {
@@ -137,5 +325,44 @@ public class SysYFormattingVisitor extends SysYParserBaseVisitor<String> {
         }
     }
 
-    // And many more methods for all other syntax elements
+    @Override
+    public String visitRelExp(SysYParser.RelExpContext ctx) {
+        if (ctx.getChildCount() == 1) {
+            return visit(ctx.addExp());
+        } else {
+            return visit(ctx.relExp()) + " " + ctx.getChild(1).getText() + " " + visit(ctx.addExp());
+        }
+    }
+
+    @Override
+    public String visitEqExp(SysYParser.EqExpContext ctx) {
+        if (ctx.getChildCount() == 1) {
+            return visit(ctx.relExp());
+        } else {
+            return visit(ctx.eqExp()) + " " + ctx.getChild(1).getText() + " " + visit(ctx.relExp());
+        }
+    }
+
+    @Override
+    public String visitLAndExp(SysYParser.LAndExpContext ctx) {
+        if (ctx.getChildCount() == 1) {
+            return visit(ctx.eqExp());
+        } else {
+            return visit(ctx.lAndExp()) + " && " + visit(ctx.eqExp());
+        }
+    }
+
+    @Override
+    public String visitLOrExp(SysYParser.LOrExpContext ctx) {
+        if (ctx.getChildCount() == 1) {
+            return visit(ctx.lAndExp());
+        } else {
+            return visit(ctx.lOrExp()) + " || " + visit(ctx.lAndExp());
+        }
+    }
+
+    @Override
+    public String visitConstExp(SysYParser.ConstExpContext ctx) {
+        return visit(ctx.exp());
+    }
 }
