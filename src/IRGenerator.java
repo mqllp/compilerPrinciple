ba16ebf9471406e14ca756c21c9ec4c5a2cab1a4
tracer-compiler2022 <compilers@software.nju.edu.cb>
2025-04-17 18:49:01 +0800

    > make 231250190 asus Linux asus-VMware-Virtual-Platform 6.11.0-21-generic #21~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Mon Feb 24 16:52:15 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:49:01 up  3:34,  1 user,  load average: 2.73, 1.82, 1.13 e1975edda3a4798658e29d907293e5b4a03282ca

--- src/IRGenerator.java
+++ src/IRGenerator.java
@@ -344,13 +344,11 @@ public class IRGenerator extends SysYParserBaseVisitor<LLVMValueRef> {
         }
 
         // 处理return语句
-        if (ctx.RETURN() != null) {
-            if (ctx.exp() != null) {
-                // 有返回值的情况
-                LLVMValueRef returnValue = ctx.exp().accept(this);
-                return LLVMBuildRet(builder, returnValue);
-            } else {
-                // 无返回值的情况（void函数）
+        if(ctx.RETURN() != null){
+            if(ctx.exp() != null){
+                LLVMValueRef r = ctx.exp().accept(this);
+                return LLVMBuildRet(builder, r);
+            }else {
                 return LLVMBuildRetVoid(builder);
             }
         }
